27654c9586dc736d3fba789c5365841159d8a97c
jameskalfox <75384370+jameskalfox@users.noreply.github.com>
2023-02-22 10:39:12 +0000

    Further functionality added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import pandas
 import streamlit
+import requests
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -19,3 +20,12 @@ fruits_selected = streamlit.multiselect(
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+fruit_choice = streamlit.text_input("Write Fruit:")
+streamlit.write(fruit_choice + " selected.")
+
+streamlit.header('Fruityvice Fruit Advice!')
+fruityvice_response = requests.get(
+    "https://fruityvice.com/api/fruit/" + fruit_choice)
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
